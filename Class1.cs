598680f717056dde2549b5fe3ca7372d2d987ca5
BogaNeeraj <92246820+BogaNeeraj@users.noreply.github.com>
2021-10-22 10:02:29 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Reflection
+{
+    class Dynamic_Object
+    {
+        public Dynamic_Object() { }
+
+        public Dynamic_Object(string Msg) { }
+
+        public void First_Method() {
+            Console.WriteLine("from first object");
+        }
+
+        public void Second_Method() {
+            Console.WriteLine("from second object");
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+
+
+namespace Demo_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Reflection in c#...");
+            Type T = typeof(int);
+            Console.WriteLine("Type of Name:{0}", T.Name);
+            Console.WriteLine("Full name of the type defined :\t{0}", T.FullName);
+            Console.WriteLine("Type of Assembly in which it is defined :\t {0} ", T.Assembly);
+            Console.WriteLine("Information about base Type:\t{0}\n", T.BaseType);
+
+
+
+            Type T1 = typeof(string);
+            Console.WriteLine("Type of Name:{0}", T1.Name);
+            Console.WriteLine("Full name of the type defined :\t{0}", T1.FullName);
+            Console.WriteLine("Type of Assembly in which it is defined :\t {0} ", T1.Assembly);
+            Console.WriteLine("Information about base Type:\t{0}\n", T1.BaseType);
+
+            Dynamic_Object MyObj1 = new Dynamic_Object();
+            MyObj1.First_Method();
+            MyObj1.Second_Method();
+
+
+            Dynamic_Object MyObj2 = new Dynamic_Object();
+            MyObj2.First_Method();
+            MyObj2.Second_Method();
+
+
+
+           
+        }
+
+    }
+}
